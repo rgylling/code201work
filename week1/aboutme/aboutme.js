65b51132d9babac8d7148385f7189ea067f38b05
Randy Gylling <MikeGylling@yahoo.com>
2016-03-16 12:07:53 -0700

    fixed javascript bug that loaded javascript before my img tag on html

--- week1/aboutme/aboutme.js
+++ week1/aboutme/aboutme.js
@@ -1,4 +1,6 @@
-
+window.onload = function() {
+//Tracks how many questions user got right
+var right = 0;
 //Asks the users Name
 var user = prompt("Hello what is your name?");
 console.log("The users name is " + user);
@@ -13,10 +15,12 @@ console.log ("The users answer is " + question1);
 if (question1.toLowerCase() === "black"){
     alert("Well stupid.. You got it wrong.. Obviously my hair isn't " + question1 + ". Ready for an even harder question?")
   } else  if (question1.toLowerCase() === "pink"){
-    alert("Well.. I still hate you.. But you got it the answer right...Ready for an even harder question?")
+    alert("Well.. I still hate you.. But you got it the answer right...Ready for an even harder question?");
+    var right = right + 1;
   } else {
     alert("Learn how to answer a question stupid! Ready for the next question?")
   };
+  console.log ("User has " + right + " questions right");
 
 //Asks the user to choose a number
 var question2 = prompt("Give me a number.. Any number...");
@@ -38,7 +42,8 @@ console.log("The answer to the question is " + answer);
 /* If you answered right return alert, if user guessed too high return alert, if user guessed too low return alert */
 
 if (question5 == answer ) {
-  alert("Wow...You're ugly but you're smart.. Congrats you got the answer right!")
+  alert("Wow...You're ugly but you're smart.. Congrats you got the answer right!");
+  var right = right + 1;
 } else if (question5 > answer) {
   alert("You guessed too high !")
 } else if (question5 < answer) {
@@ -46,6 +51,7 @@ if (question5 == answer ) {
 } else {
   alert("You guess sucks!")
 };
+console.log("User has " + right + " questions right");
 
 //Asks a riddle to the user
 var question6 = prompt("Next question " + user + ". How much wood could a woodchuck chuck if a woodchuck could chuck ______?" );
@@ -53,10 +59,12 @@ console.log("The user guessed " + question6);
 
 //If riddle is correct alert else if the answer is wrong alert wrong
 if (question6 == "wood") {
-  alert("I thought the answer was 7 but apparently it is wood.. Congrats!")
+  alert("I thought the answer was 7 but apparently it is wood.. Congrats!");
+  var right = right + 1;
 } else {
   alert("Wrong answer dumbo!")
 };
+console.log("User has " + right + " questions right");
 
 //Declares a variable as true for my while loop
 var askAgain = true;
@@ -69,8 +77,14 @@ while (askAgain) {
     if (question7 == 2000 ) {
         askAgain = false;
         alert("You got the answer right jerk!");
+        var right = right + 1;
         break;
     } else
        alert("Try again stupid!");
        askAgain = true;
     };
+
+console.log("User has " + right + " questions right");
+
+alert("Congrats " + user + ". You got " + right + " questions right out of 4! Now check out my resume jerk!")
+};
